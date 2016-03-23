c40252c719c907a924e51dccc665eba42f02e088
matheus <mrosmaninhu@outlook.com>
2016-03-23 10:09:53 -0300

    Remoção do if

--- includes/js/processar.js
+++ includes/js/processar.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
     $('.processar').click(function() {
-        if ($(this).has('class', 'btn-default')) {
-            $.ajax({
-                url: 'processar.php',
-                method: 'POST',
-                success: function(retorno) {
-                    if (retorno == 1) {
-                        $('.processar span').attr('class', 'glyphicon glyphicon-ok');
-                    }
-                    else {
-                        $('.processar span').attr('class', 'glyphicon glyphicon-remove');
-                    }
-                },
-                error: function(erro) {
-                    alert('Serviço indisponivel');
+        $.ajax({
+            url: 'processar.php',
+            method: 'POST',
+            success: function(retorno) {
+                if (retorno == 1) {
+                    $('.processar span').attr('class', 'glyphicon glyphicon-ok');
                 }
-            });
-        }
+                else {
+                    $('.processar span').attr('class', 'glyphicon glyphicon-remove');
+                }
+            },
+            error: function(erro) {
+                alert('Serviço indisponivel');
+            }
+        });
     })
 })
